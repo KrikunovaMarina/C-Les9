817c34a31d6a73463677533b6c2417e52935ee3a
Marina <umarishka@bk.ru>
2022-09-16 23:13:58 +0300

    add N64 N66 N68

---
+++ N64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine($"Все натуральные числа в промежутке от {n} до {m}:");
+
+if (n<m)
+{
+for (int i = n; i <= m; i++)
+    Console.Write($" {i}");
+for (int i = m; i <= n; i++)
+     Console.Write($" {i}");
+}
+
---
+++ N66/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine($"Все натуральные числа в промежутке от {n} до {m}:");
+
+int result = 0;
+if (n<m)
+{
+for (int i = n; i <= m; i++)
+    Console.Write($" {i}");
+for (int i = m; i <= n; i++)
+     Console.Write($" {i}");
+}
+for (int i = 0; i <= m; i++)
+{
+	result += i;
+}
+
+Console.Write(" -> сумма натуральных чисел равна " + result);
+
